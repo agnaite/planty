7aa31a2dbc3aeff4dce4bcf1309292382ddd4ea9
Agne Klimaite <agnaite@me.com>
2016-08-09 20:37:17 +0000

    added custom css

--- model.py
+++ model.py
@@ -101,6 +101,6 @@ if __name__ == "__main__":
 
     # Create our tables and some sample data
     db.create_all()
-    example_data()
+    #example_data()
 
     print "Connected to DB."
--- server.py
+++ server.py
@@ -1,18 +1,17 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask, render_template
-from jinja2 import StrictUndefined
 from model import connect_to_db, db, Plant, User, PlantUser
-from flask_debugtoolbar import DebugToolbarExtension
 import secret
 
 app = Flask(__name__)
 app.secret_key = secret.APP_KEY
-app.jinja_env.undefined = StrictUndefined
 
 
 @app.route('/')
 def index_page():
     """Show index page."""
-
+    print "hey"
     return render_template("index.html")
 
 
@@ -20,7 +19,9 @@ def index_page():
 def show_all_plants_by_name():
     """Show all plants by name."""
 
+    print "\n\n\n\n\nSTARTED QUERY\n\n"
     all_plants = Plant.query.all()
+    print "\n\n\nFINISHED QUERY\n\n"
 
     return render_template("plants_by_name.html", plants=all_plants)
 
@@ -30,6 +31,4 @@ if __name__ == "__main__":
     connect_to_db(app)
     app.debug = True
 
-    DebugToolbarExtension(app)
-
-    app.run()
+    app.run(host="0.0.0.0")
