43775da61d3d061e0fbb16868c9aa8d2226677ea
Agnė Klimaitė <agnaite@me.com>
2016-09-06 15:02:22 -0700

    fixed file naming error caused by nose

--- tests/integration-tests.py
+++ tests/integration-tests.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
-from flask.ext.testing import LiveServerTestCase
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 import unittest
-from server import app
-from model import Plant, connect_to_db, db, example_data
 
 
 class IntegrationTests(unittest.TestCase):
@@ -14,19 +10,19 @@ class IntegrationTests(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.PhantomJS()
+        self.driver.get("http://localhost:5000")
 
     def test_homepage_loads(self):
         """Checks if homepage loads"""
 
         driver = self.driver
-        driver.get("http://localhost:5000")
+
         self.assertIn("planty", driver.title)
 
     def test_search_for_plant(self):
         """Checks if able to search for a plant by name"""
 
         driver = self.driver
-        driver.get("http://localhost:5001")
 
         element = driver.find_element_by_name("plant-name")
         element.send_keys("orchid")
