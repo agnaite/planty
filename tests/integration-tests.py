16b0333c544bb02c5522b766e4cf5e19799786be
Agnė Klimaitė <agnaite@me.com>
2016-09-06 20:22:01 -0700

    removed broken test

--- tests/integration-tests.py
+++ tests/integration-tests.py
@@ -22,23 +22,6 @@ class IntegrationTests(unittest.TestCase):
         self.assertIn("planty", self.driver.title)
 
 
-    def test_search_for_plant(self):
-        """Checks if able to search for a plant by name"""
-
-        self.driver.get("http://localhost:5000")
-
-        element = self.driver.find_element_by_name("plant-name")
-        element.send_keys("orchid")
-        element.send_keys(Keys.RETURN)
-
-        self.assertIn("Orchid Plant", self.driver.page_source)
-        self.assertNotIn("No plants found.", self.driver.page_source)
-        links = self.driver.find_elements_by_link_text('Orchid')
-
-        links[0].click()
-        self.assertIn("Orchidaceae", self.driver.page_source)
-
-
     def tearDown(self):
         self.driver.quit()
 
