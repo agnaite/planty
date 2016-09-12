bfdd1f003c309dc8ef1d37d08c01ac6d73a2e4dc
Agne Klimaite <agnaite@me.com>
2016-09-12 03:54:21 +0000

    connect to database_url

--- model.py
+++ model.py
@@ -157,7 +157,7 @@ def connect_to_db(app, db_uri=None):
 if __name__ == "__main__":
 
     from server import app
-    connect_to_db(app)
+    connect_to_db(app, os.getenv('DATABASE_URL'))
 
     # Create our tables and some sample data
     db.create_all()
