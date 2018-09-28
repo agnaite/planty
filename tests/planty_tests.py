0f241cc2cce9f87bd013f11bc9d14b48fb8fad41
Agne Klimaite <agnaite@me.com>
2018-09-28 14:48:48 -0700

    add search and index test

--- tests/planty_tests.py
+++ tests/planty_tests.py
@@ -1,12 +1,12 @@
 import os
 import sys
-import bcrypt 
+import bcrypt
 
 topdir = os.path.join(os.path.dirname(__file__), "..")
 sys.path.append(topdir)
 
 from planty import app
-from planty.models import db, connect_to_db, User
+from planty.models import db, connect_to_db, User, Plant
 from flask import session
 import unittest
 
@@ -25,43 +25,69 @@ class PlantyTestCase(unittest.TestCase):
         db.drop_all()
         db.engine.dispose()
 
+    def test_index(self):
+        result = self.client.get('/')
+
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b'planty', result.data)
+
+    def test_search(self):
+        # with no results
+        result = self.client.post('/search',
+                                  data={'name': 'planty',
+                                        'filters': ''},
+                                  follow_redirects=True)
+
+        self.assertEqual(b'None', result.data)
+
+        # with results
+        result = self.client.post('/search',
+                                  data={'name': 'ro',
+                                        'filters': ''},
+                                  follow_redirects=True)
+
+        self.assertIn(b'rose', result.data)
+
     def test_login_and_logout(self):
         with self.client as c:
             # user does not exist
             result = c.post('/process_login',
                             data={'username': 'hacker', 'password': 'hack0r'},
                             follow_redirects=True)
-            
+
             self.assertEqual(result.status_code, 200)
             self.assertEqual(b'error', result.data)
             self.assertTrue('logged_in' not in session)
-        
+
             # user exists
             result = c.post('/process_login',
                             data={'username': 'agne', 'password': 'password'},
                             follow_redirects=True)
-            
-            
+
+
             self.assertEqual(result.status_code, 200)
             self.assertTrue('logged_in' in session)
             self.assertEqual(session['logged_in'], 1)
-    
+
             result = c.get('/process_logout')
-            
+
             self.assertEqual(b'logged out', result.data)
             self.assertTrue('logged_in' not in session)
 
 def seed_test_data():
-    user = User(username='agne', 
+    user = User(username='agne',
                 password=bcrypt.hashpw('password'.encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
                 email='agne@gmail.com',
                 first_name='agne',
                 last_name='klimaite',
                 phone='4079139090')
 
+    plant = Plant(name='rose')
+
+    db.session.add(plant)
     db.session.add(user)
     db.session.commit()
-       
+
 if __name__ == '__main__':
     unittest.main()
 
